95fe0bbb463b7fd582384c5790aea0d75ff6ee2d
syou97 <sumita@rish.style>
2025-04-02 13:48:12 +0900

    設定: GitHub Pages用のbasePath追加

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out'  // 出力ディレクトリを明示的に指定
+  basePath: '/yuri',
+  assetPrefix: '/yuri/',
+  // 他の設定があればそのまま残す
 }
 
 module.exports = nextConfig
